2f05e1a27dd20272589b195b6f11900ae93610c3
keenalevine <keenalevine97@gmail.com>
2023-03-08 17:24:10 +0800

    initial commit

--- libs/api.ts
+++ libs/api.ts
@@ -19,6 +19,5 @@ export const graphqlClient = new GraphQLClient(endPoint, {
   headers: {
     "Content-Type": "application/json",
     "X-Shopify-Storefront-Access-Token": accessToken,
-    "Access-Control-Allow-Origin": "*",
   },
 });
