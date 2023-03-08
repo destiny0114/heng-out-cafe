a5c073de48272d81ae46d123653b0fe14cb3be60
keenalevine <keenalevine97@gmail.com>
2023-03-08 22:09:33 +0800

    initial commit

--- libs/api.ts
+++ libs/api.ts
@@ -1,7 +1,7 @@
 import { QueryClient } from "@tanstack/react-query";
 import { GraphQLClient } from "graphql-request";
 
-const publicEndPoint = process.env.NEXT_PUBLIC_PREFIX_ENDPOINT || "";
+const publicEndPoint = process.env.NEXT_PUBLIC_API_ENDPOINT || "";
 const apiEndPoint = process.env.SHOPIFY_STORE_ENDPOINT || "";
 const accessToken = process.env.SHOPIFY_ACCESS_TOKEN || "";
 
