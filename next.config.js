1ace2ce74c5b8c7490b32aabdb3dae74e6340ab8
keenalevine <keenalevine97@gmail.com>
2023-03-08 17:14:37 +0800

    initial commit

--- next.config.js
+++ next.config.js
@@ -22,7 +22,7 @@ const nextConfig = {
     return [
       {
         // matching all API routes
-        source: "/*",
+        source: "https://heng-out-cafe.myshopify.com/api/2022-10/graphql",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
